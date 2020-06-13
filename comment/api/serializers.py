9b3f4303b5e5abcecbd77c08c1dba7162f236aee
mgunes <mistafagunes@gmail.com>
2020-06-13 22:00:48 +0300

    created nested serializers

--- comment/api/serializers.py
+++ comment/api/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from rest_framework import serializers
 from comment.models import Comment
+from django.contrib.auth.models import User
+from post.models import Post
 
 
 class CommentCreateSerializer(ModelSerializer):
@@ -16,11 +18,33 @@ class CommentCreateSerializer(ModelSerializer):
 
 
 
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']
+
+
+
+class PostSerializer(ModelSerializer):
+    username = serializers.SerializerMethodField()
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'user', 'username']
+    
+    def get_username(self, obj):
+        return obj.user.username
+        
+
+
 class CommentListSerializer(ModelSerializer):
     replies = SerializerMethodField()
+    user = UserSerializer()
+    post = PostSerializer()
     class Meta:
         model = Comment
         fields = '__all__'
+        # depth = 1 # ForeignKey li olan herseyin icin aciyor
+
 
     def get_replies(self, obj):
         if obj.any_children:
