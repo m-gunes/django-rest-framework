bd4ac315dc37e6c45843805a734510739e7a2431
mgunes <mistafagunes@gmail.com>
2020-06-16 21:00:40 +0300

    added jwt - rest_framework_simplejwt

--- blog/settings.py
+++ blog/settings.py
@@ -135,3 +135,9 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
