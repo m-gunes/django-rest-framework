5b08584989cfa632bc4acbb34b7e3ebcac694941
mgunes <mistafagunes@gmail.com>
2020-06-16 11:16:54 +0300

    moved api folder in post folder

--- blog/urls.py
+++ blog/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/post/', include('api.urls')),
+    path('api/post/', include('post.api.urls')),
     path('api/comment/', include('comment.api.urls')),
     path('api/favorite/', include('favorite.api.urls'))
 ]
